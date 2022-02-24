576c92fd3080bdb049bee6b335c66c846bee656a
Tatyana Lysenko <tatyana_lysenko@mail.ru>
2022-02-24 19:46:06 +0300

    changed the length of list "prices"

--- lesson2/task2_5.py
+++ lesson2/task2_5.py
@@ -1,16 +1,18 @@
 import random
 
-prices = [round(random.uniform(1, 2000), 2) for i in range(50)]
+prices = [round(random.uniform(1, 2000), 2) for i in range(20)]
 print(prices)
 print(f'id первоначального списка: {id(prices)}')
 prices.sort()
-prices_new = sorted(prices, reverse=True)
+print(prices)
 print(f'id отсротированного по возрастанию списка: {id(prices)}')
+prices_new = sorted(prices, reverse=True)
+print(prices_new)
 for price in prices:
     price = str(price)
     price_part = price.split('.')
     print(f'{int(price_part[0]):02d} руб {int(price_part[1]):02d} коп', end=', ')
-print('')
+print()
 print('5 самых дорогих товаров:')
 
 prices_new = prices_new[:5]
